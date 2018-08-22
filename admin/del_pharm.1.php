6ab7221796568202901189157c3a570358e3c7ed
tomkeith <tomah5714@gmail.com>
2018-08-22 14:58:34 +0300

    administrator functionalities implemented

--- admin/del_pharm.1.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-	include('connect_db.php');
-	$id=$_GET['stock_id'];
-	$result = $dbh->prepare("DELETE FROM stock WHERE stock_id= :memid");
-	$result->bindParam(':memid', $id);
-	$result->execute();
-	echo "<script type='text/javascript'> document.location = 'admin_stock.php'; </script>";
-?>
